b07538689c2ac084b7a301943bb97aff1c322a7f
harendrashriffle <harendras@shriffle>
2023-12-05 19:25:07 +0530

    Make users active admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,10 +2,8 @@ ActiveAdmin.register User do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :email, :encrypted_password, :type, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :mobile, :address
+
+  permit_params :name, :email, :encrypted_password, :type, :mobile, :address
   #
   # or
   #
@@ -15,4 +13,16 @@ ActiveAdmin.register User do
   #   permitted
   # end
   
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :email
+      f.input :encrypted_password
+      f.input :mobile
+      f.input :address
+      f.input :type
+    end
+    f.actions
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable,  stretches: 13
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  # has_secure_password
 
   validates :name, length: {minimum: 3}, presence: true
   validates :email, uniqueness: {case_sensitive: false}, presence: true
   # validates :encrypted_password, length: {minimum: 4}
   validates :type, inclusion: { in: ['Owner', 'Customer']}
   validates :mobile, length: {is: 10}, uniqueness: true, presence: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["address", "confirmation_sent_at", "confirmation_token", "confirmed_at", "created_at", "email", "encrypted_password", "id", "mobile", "name", "remember_created_at", "reset_password_sent_at", "reset_password_token", "type", "unconfirmed_email", "updated_at"]
+  end
 end
